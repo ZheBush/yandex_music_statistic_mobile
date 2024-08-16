518db06a06f74825b5d1f151ca0f251b8e4b1312
Bushkov Evgeniy <buschkow2005@gmail.com>
2024-08-17 00:13:23 +0400

    hello

---
+++ App/app/src/main/java/com/example/app/HomeActivity.kt
@@ -0,0 +1,29 @@
+package com.example.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun Home() {
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun HomePreview() {
+    Auth()
+}
---
+++ App/app/src/main/java/com/example/app/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Orange = Color(red = 255, green = 160, blue = 0)
+val DarkOrange = Color(red = 150, green = 90, blue = 0)
+
+val DarkGray = Color(red = 30, green = 30, blue = 30)
+val Gray = Color(red = 100, green = 100, blue = 100)
